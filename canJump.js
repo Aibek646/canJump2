0ebb4347a9b7d9b9bdf735222331be060ee8677a
Aibek Ramazanov <aibekramazanov@Aibeks-MacBook-Pro.local>
2025-07-15 13:27:21 +0500

    greedy solution

--- canJump.js
+++ canJump.js
@@ -16,3 +16,19 @@ var jump = function (nums) {
 
   return jumps;
 };
+
+var jumpp = function (nums) {
+  let totalJumps = 0;
+  let currentJump = 0;
+  let farthestJump = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    farthestJump = Math.max(farthestJump, i + nums[i]);
+    if (i === currentJump) {
+      totalJumps++;
+      currentJump = farthestJump;
+    }
+  }
+  return totalJumps;
+};
+
+const num = [2, 3, 1, 1, 4];
